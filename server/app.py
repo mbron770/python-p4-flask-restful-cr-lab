4ed1e95988a1ab2f0e994c32fc100c0a964abd4b
mbron770 <mbronfin.tsd@gmail.com>
2023-07-28 00:45:22 -0400

    c

--- server/app.py
+++ server/app.py
@@ -17,10 +17,32 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self): 
+        all = Plant.query.all()
+        plants = []
+        for plant in all: 
+            plants.append(plant.to_dict())
+        return (plants), 200
+    def post(self): 
+        plant = Plant()
+        data = request.json
+        try:
+            for attr in data: 
+                setattr(plant, attr, data[attr])
+            db.session.add(plant)
+            db.session.commit()
+            return plant.to_dict, 201
+        except(IntegrityError, ValueError) as ie: 
+            return {'error': ie.args}, 422
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id): 
+        plant = Plant.query.filter(Plant.id == id).first()
+        return plant.to_dict(), 200
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
